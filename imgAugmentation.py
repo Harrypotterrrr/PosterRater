c32613ea1ffc7e96567cb59718858ba4b2b75545
Harrypotterrrr <jiahaolin19971119@126.com>
2019-01-31 22:54:55 +0800

    :palm_tree: update multi-process solution

--- imgAugmentation.py
+++ imgAugmentation.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
 """Data Aumentation
    1. 翻转变换 flip
    2. 随机修剪 random crop
@@ -13,6 +19,7 @@ from PIL import Image, ImageEnhance, ImageOps, ImageFile
 import numpy as np
 import random
 import threading, os, time
+from multiprocessing import Pool
 
 # import logging
 # logger = logging.getLogger(__name__)
@@ -35,6 +42,26 @@ class ImageProcess:
     @staticmethod
     def saveImage(image, path):
         image.save(path)
+        
+    @staticmethod
+    def checkDir(src_path, dst_path):
+        if not os.path.isdir(src_path):
+            print(src_path, "is not directory!")
+            return False
+        if not os.path.isdir(dst_path):
+            os.mkdir(dst_path)
+            print("make new", dst_path, "path!")   
+        return True
+        
+    @staticmethod
+    def imageResize(image, width=200, height=280, mode=Image.BICUBIC):
+        """
+        resize the image to specific size
+        :param mode 邻近插值,双线性插值,双三次B样条插值(default)
+        :param image PIL class image
+        :return: image after resize
+        """
+        return image.resize((width, height), mode)
 
     @staticmethod
     def randomRotation(image, mode=Image.BICUBIC):
@@ -44,7 +71,7 @@ class ImageProcess:
         :param image PIL class image
         :return: image after rotation
         """
-        random_angle = np.random.randint(1, 360)
+        random_angle = np.random.randint(0, 360)
         return image.rotate(random_angle, mode)
 
     @staticmethod
@@ -64,7 +91,8 @@ class ImageProcess:
             (image_width + crop_win_size) >> 1,
             (image_height + crop_win_size) >> 1
         )
-        return image.crop(random_region)
+        image = image.crop(random_region)
+        return ImageProcess.imageResize(image)
 
 
     @staticmethod
@@ -132,51 +160,119 @@ class ImageProcess:
         img[:, :, 2] = img_b.reshape([width, height])
         return Image.fromarray(np.uint8(img))
 
+    @staticmethod
+    def imgProcess(file, src_path, dst_path, times=20):
+        """
+        image process for each file
+        """
+        opsList = {"randomRotation", "randomCrop", "randomColor", "randomGaussian"}
+        funcMap = {"randomRotation": ImageProcess.randomRotation,
+               "randomCrop": ImageProcess.randomCrop,
+               "randomColor": ImageProcess.randomColor,
+               "randomGaussian": ImageProcess.randomGaussian
+               }
 
-def oneProcess(file, src_path, dst_path, times=20):
-    
-    opsList = {"randomRotation", "randomCrop", "randomColor", "randomGaussian"}
-    funcMap = {"randomRotation": ImageProcess.randomRotation,
-           "randomCrop": ImageProcess.randomCrop,
-           "randomColor": ImageProcess.randomColor,
-           "randomGaussian": ImageProcess.randomGaussian
-           }
-    
-    file_path = src_path + file
-    if not os.path.isfile(file_path):
-        print("image doesn't exist!")
-        return
-    im = ImageProcess.openImage(file_path)
-    im_name = file.split('.')[0]
+        file_path = src_path + file
+        if not os.path.isfile(file_path):
+            print("image doesn't exist!")
+            return
+        im = ImageProcess.openImage(file_path)
+        im_name = file.split('.')[0]
 
-    for i in range(times):
-        im_cur = im
-        for func in funcMap.values():
-            im_cur = func(im_cur)
-        ImageProcess.saveImage(im_cur, dst_path+im_name+"_"+str(i)+".png")
+        for i in range(times):
+            im_cur = im
+            for func in funcMap.values():
+                im_cur = func(im_cur)
+            ImageProcess.saveImage(im_cur, dst_path+im_name+"_"+str(i)+".png")
+        print(file+" process finish!")
+    
+    @staticmethod
+    def multiProcess(src_path, dst_path, times=20):
+        """
+        The solution of multi-processs
+        """
+        if ImageProcess.checkDir(src_path, dst_path) is False:
+            return
         
+        files = os.listdir(src_path)
+        print("file's count:", len(files))
+        pool = Pool()
+        for file in files:
+            pool.apply_async(ImageProcess.imgProcess, args=(file, src_path, dst_path, times))
+        pool.close()
+        pool.join()
 
-def threadProcess(src_path, dst_path):
-    if not os.path.isdir(src_path):
-        print("src_path is not directory!")
-        return
-    if not os.path.isdir(dst_path):
-        os.mkdir(dst_path)
-        print("make new destination path!")
-        return
-    
-#     root, dirs, files = os.walk(src_path)
-    files = os.listdir(src_path)
+    @staticmethod
+    def multiThread(src_path, dst_path, times=20):
+        """
+        The solution of multi-thread
+        """
+        if ImageProcess.checkDir(src_path, dst_path) is False:
+            return
 
-    
-    print(len(files))
-    for file in files:
-        t = threading.Thread(target=oneProcess, args=(file, src_path, dst_path, ))
-        print(file + " is processing...")
-        t.start()
-    t.join()
-        
+    #     root, dirs, files = os.walk(src_path)
+        files = os.listdir(src_path)
+
+        print(len(files))
+        for file in files:
+            t = threading.Thread(target=ImageProcess.imgProcess, args=(file, src_path, dst_path, times))
+    #         t.setDaemon(True)
+            print(file + " is processing...")
+            t.start()
+            time.sleep(0.2)
+        t.join()
+
+    @staticmethod
+    def singleThread(src_path, dst_path, times=20):
+        """
+        The solution of single-process/thread
+        """
+        if ImageProcess.checkDir(src_path, dst_path) is False:
+            return
+
+    #     root, dirs, files = os.walk(src_path)
+        files = os.listdir(src_path)
+
+        print(len(files))
+        for file in files:
+            print(file + " is processing...")
+            ImageProcess.imgProcess(file, src_path, dst_path, times)
+
+
+# In[13]:
+
 
 if __name__ == '__main__':
-    threadProcess(r"./data/poster_positive/", r"./data/tmp/")
+    
+    src_path = "./data/poster_negative/"
+    dst_path = "./data/tmp/"
+    
+    ImageProcess.multiProcess("./data/poster_positive/", "./data/tmp/", 5)
+#     multiThread(r"./data/poster_positive/", r"./data/tmp/")
+#     singleThread(r"./data/poster_negative/", r"./data/tmp2/")
+    
     print("end")
+
+
+# In[ ]:
+
+
+path = './data/poster_positive/'
+test_pic_list = os.listdir(path)
+print(len(test_pic))
+test_im = Image.open(path+test_pic[0])
+print(np.array(test_im).shape)
+opsList = {"randomRotation", "randomCrop", "randomColor", "randomGaussian"}
+funcMap = {"randomRotation": ImageProcess.randomRotation,
+       "randomCrop": ImageProcess.randomCrop,
+       "randomColor": ImageProcess.randomColor,
+       "randomGaussian": ImageProcess.randomGaussian
+       }
+
+im_name = test_pic[0].split('.')[0]
+
+im_cur = test_im
+for key, func in funcMap.items():
+    im_cur = func(im_cur)
+    print(key, np.array(im_cur).shape)
+
